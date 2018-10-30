03d948debd574340628e8e8d6105930b785ae516
Jared M. Smith <absynce@gmail.com>
2018-10-30 14:53:06 -0500

    Upgrade to work with Elm 0.19
    Maintain backward compatibility with Elm <= 0.18.

--- index.js
+++ index.js
@@ -9,8 +9,19 @@
                     ports: '&'
                 },
                 link: function (scope, element) {
-                    var app = scope.src.embed(element[0], scope.flags);
+                    var app = {};
                     var cleanUp = angular.noop;
+                    var isElm0_19 = scope.src.init !== undefined;
+
+                    if (isElm0_19) {
+                        app = scope.src.init({
+                            node: element[0],
+                            flags: scope.flags
+                        });
+                    }
+                    else { // Elm 0.18 or lower.
+                        app = scope.src.embed(element[0], scope.flags);
+                    }
 
                     if (scope.ports !== undefined) {
                         cleanUp = scope.ports({ ports: app.ports });
